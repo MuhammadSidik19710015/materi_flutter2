ad8304d82d6e37f9f99bfcab7072b183b031733d
sidik <sidikmuh25@gmail.com>
2021-10-08 14:14:13 +0800

    first commit : video praktek 4

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actionsIconTheme:
+              IconThemeData(size: 30.0, color: Colors.black, opacity: 10.0),
+          leading: Icon(Icons.menu),
+          backgroundColor: Colors.blue,
+          title: Text('Hello Appbar',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          actions: [
+            Icon(Icons.search),
+            Icon(Icons.more_vert),
+          ],
+        ),
+        body: Container(
+          color: Colors.blue,
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          padding: EdgeInsets.all(30),
+          child: Icon(
+            Icons.home,
+            color: Colors.red,
+            size: 1000.78,
+          ),
+        ));
+  }
+}
